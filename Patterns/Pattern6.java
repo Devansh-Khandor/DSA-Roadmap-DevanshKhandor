c8a92b9e1384dc65484362645ebd7558f54670e7
Devansh-Khandor <devanshkhandor1234@gmail.com>
2026-07-01 15:39:21 +0530

    Pattern Update-6

--- Patterns/Pattern6.java
+++ Patterns/Pattern6.java
@@ -1,12 +1,16 @@
+import java.util.*;
+
 public class Pattern6 {
     public static void main(String args[]){
-        int c = 1;
-        for(int i=1;i<=5;i++){
-            for(int j=5;j>=i;j--){
-                System.out.print(c);
-                c++;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int n = sc.nextInt();
+
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
             }
-            c=1;
             System.out.println();
         }
     }
