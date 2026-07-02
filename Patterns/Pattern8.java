12fd20cb4f14a9edeb6c6fff396045c9dab40541
Devansh-Khandor <devanshkhandor1234@gmail.com>
2026-07-02 13:36:12 +0530

    Pattern 8

---
+++ Patterns/Pattern8.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Pattern8 {
+    public static void main(String args[]){
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        int n = ob.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=2*n-1;j++){
+                if(j>=i && j<=(2*n)-i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
