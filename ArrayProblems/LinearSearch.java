8a50e92a3e19f25e44b1b2d599354ac8e73868fd
Devansh-Khandor <devanshkhandor1234@gmail.com>
2026-07-03 15:57:08 +0530

    Array - Linear Search

---
+++ ArrayProblems/LinearSearch.java
@@ -0,0 +1,39 @@
+package ArrayProblems;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    @SuppressWarnings("resource")
+    public static void main(String args[]){
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter the number of elements in your array");
+        int n = ob.nextInt(); 
+        int arr[] = new int[n]; 
+        System.out.println("Start entering your elements");
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print("Element "+(i+1)+": ");
+            arr[i] = ob.nextInt();
+            System.out.println();
+        }
+        System.out.println("Enter the element you want to search");
+        int x = ob.nextInt();
+        int result = linearsearch(arr, x);
+        if(result == -1){
+            System.out.println("Element not found in the array");
+        } else {
+            System.out.println("Element found at index: "+result);    
+        }
+        
+    }
+    public static int linearsearch(int arr[], int x) {
+        // code here
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
