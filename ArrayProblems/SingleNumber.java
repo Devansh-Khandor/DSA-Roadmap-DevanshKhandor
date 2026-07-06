51af13afbfa79513cf6301b90170ef725ced79e1
Devansh-Khandor <devanshkhandor1234@gmail.com>
2026-07-06 14:59:40 +0530

    Array - SingleNumber

---
+++ ArrayProblems/SingleNumber.java
@@ -0,0 +1,31 @@
+package ArrayProblems;
+
+import java.util.Scanner;
+
+public class SingleNumber {
+    public static int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0;i<nums.length;i++){
+            xor = xor^nums[i];
+        }
+        return xor;
+    }
+    @SuppressWarnings("resource")
+    public static void main(String args[]){
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter the number of elements in your array");
+        int n = ob.nextInt(); // Read number of elements
+        int arr[] = new int[n]; // Declare array of size n
+        System.out.println("Start entering your elements");
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print("Element "+(i+1)+": ");
+            arr[i] = ob.nextInt();
+            System.out.println();
+        }
+
+        System.out.println("The largest element in the array is: "+singleNumber(arr));
+    }
+
+
+}
